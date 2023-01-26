34a80b08e7c202403138c3ea3e08849f67321a52
AlejandroDelg <alexdelga2013@gmail.com>
2023-01-26 12:12:12 -0700

    Creado proyecto de spring y añadido primer documento HTML

---
+++ IberComponents/src/main/java/es/codeurjc/ibercomponents/IberComponentsApplication.java
@@ -0,0 +1,13 @@
+package es.codeurjc.ibercomponents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IberComponentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IberComponentsApplication.class, args);
+	}
+
+}
