1b411231d5f261520af74b05d74a21676ca9b50a
AlejandroDelg <alexdelga2013@gmail.com>
2023-01-31 17:23:05 -0700

    Añadido los archivos antiguos

---
+++ IberComponents/src/test/java/es/code/urjc/ibercomponents/IberComponentsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.code.urjc.ibercomponents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IberComponentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
