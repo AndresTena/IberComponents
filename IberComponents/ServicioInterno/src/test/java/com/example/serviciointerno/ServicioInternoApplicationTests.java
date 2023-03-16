b3e593f5ec30c94f0049bce38f1a00d910ad5e52
AlejandroDelg <alexdelga2013@gmail.com>
2023-03-16 12:45:35 +0100

    Creado nuevo proyecto para servicio interno

---
+++ IberComponents/ServicioInterno/src/test/java/com/example/serviciointerno/ServicioInternoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviciointerno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioInternoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
